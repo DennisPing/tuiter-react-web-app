1d3aa4ef1d1bd148893980f96bf9c6fffb928d87
Dennis Ping <idennisping@gmail.com>
2022-10-16 19:56:21 -0400

    Finished middle column

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,25 +1,32 @@
 const PostSummaryItem = (post) => {
+  // Conditional check if num Tweets exists
+  let tweets = "";
+  if (post.tweets) {
+    tweets = `<div class="text-secondary">${post.tweets} Tweets</div>`;
+  }
+
   return `
-    <li class="list-group-item">
-    <div class="row">
-        <div class="col-9 col-md-10 px-2">
-            <div class="text-secondary">
-                ${post.topic}
-            </div>
-            <div>
-                <span class="fw-bolder">${post.userName}</span>
-                <i class="fa-solid fa-circle-check"></i>
-                <span class="text-secondary"> - ${post.time} </span>
-            </div>
-            <div class="fw-bold">
-                ${post.title}
-            </div>
-        </div>
-        <div class="col-3 col-md-2 align-self-center">
-            <img src=${post.image} class="rounded-3 float-end" height="80px">
-        </div>
-    </div>
-    </li>
+    <a href="#" class="list-group-item list-group-item-action">
+      <div class="row">
+          <div class="col-9 col-md-10 px-2">
+              <div class="text-secondary">
+                   ${post.topic}
+              </div>
+              <div>
+                  <span class="fw-bolder">${post.userName}</span>
+                  <i class="fa-solid fa-circle-check"></i>
+                  <span class="text-secondary"> - ${post.time} </span>
+              </div>
+              <div class="fw-bold">
+                  ${post.title}
+              </div>
+              ${tweets}
+          </div>
+          <div class="col-3 col-md-2 align-self-center">
+              <img src=${post.image} class="rounded-3 float-end" height="80px">
+          </div>
+      </div>
+    </a>
     `;
 };
 export default PostSummaryItem;
--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,15 +1,16 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import WhoToFollowList from "../WhoToFollowList/index.js";
-import PostSummaryList from "../PostSummaryList/index.js";
+// import PostSummaryList from "../PostSummaryList/index.js";
+import ExploreComponent from "../ExploreScreen/ExploreComponent.js";
 
-function exploreComponent() {
+function explorePage() {
   $("#wd-explore").append(`
         <div class="row mt-2">
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar()}
             </div>
             <div class="col-10 col-lg-7 col-xl-6">
-                ${PostSummaryList()}
+                ${ExploreComponent()}
             </div>
             <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 ${WhoToFollowList()}
@@ -18,4 +19,4 @@ function exploreComponent() {
     `);
 }
 
-$(exploreComponent);
+$(explorePage);
