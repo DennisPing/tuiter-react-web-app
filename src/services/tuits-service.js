d880531bf2b86ba0dd6fa13705ed78e604e0cbb6
Dennis Ping <idennisping@gmail.com>
2022-11-09 18:01:23 -0500

    Set the API route for local dev and remote prod

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const TUITS_API = "http://localhost:4000/api/tuits";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const findTuits = async () => {
   const resp = await axios.get(TUITS_API);
