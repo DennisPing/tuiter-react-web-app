a97b58ff74fd66f3d19ead0c2d76f6cc0ba3532b
Dennis Ping <idennisping@gmail.com>
2022-11-24 13:14:46 -0500

    Use prefix REACT_APP_

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE = process.env.REACT_APP_API_BASE;
-const API_BASE = process.env.RENDER_BACKEND_API;
+const API_BASE = process.env.REACT_APP_API_RENDER;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const findTuits = async () => {
